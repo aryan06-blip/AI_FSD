a7ee8be2415bc565b39539d2075c50b834ee6771
aryan06-blip <aryan.2428cseaiml273@kiet.edu>
2026-05-17 23:22:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,10 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
     
-    app.get('*', (req, res) => {
+        app.use((req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
     });
+
 }
 
 // Database Connection
